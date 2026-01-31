b7a97e3c0cc42045b1e950f2571f08dd00b943bc
caseysims <caseysims07@gmail.com>
2026-01-31 17:56:21 +0000

    Switching Accounts Now work, used claude ai to help understand the accountsList functions and how objects work in them

--- src/Banking.java
+++ src/Banking.java
@@ -7,20 +7,28 @@ public class Banking
     static Accounts account;
     static Scanner input;
     static int menuI;
-    static ArrayList<Accounts> accountsList;
+
+    static ArrayList<Accounts> accountsList = new ArrayList<>();
 
     public Banking()
     {
+
+
         input = new Scanner(System.in);
-        accountsList = new ArrayList<>();
+
+
     }
 
     public static void main(String[] args)
     {
-
+        String inname = "JohnClyde";
+        account = new Accounts(inname);
+        accountsList.add(account);
 
         Banking bank = new Banking();
         bank.run();
+
+
     } //main function
 
 
@@ -28,90 +36,100 @@ public class Banking
     public void createAccount()
     {
         System.out.println("Welcome to WIS Banking, Please type in your name:");
-        input.nextLine();
         String name = input.nextLine();
         account = new Accounts(name);
         accountsList.add(account);
 
-        System.out.println("Your Details are as follows. \n Account Number: " + account.account_No);
+        System.out.println("Your Details are as follows. \nAccount Number: " + account.account_No);
     }//create account function
 
     public void run()
     {
 
-        if(account == null)
+
+        if(login() == true)
         {
-            createAccount();
-        }
-
-        System.out.println("Welcome to WIS Banking please login below or press 0 if new. \n Account Number:");
-        menuI = input.nextInt();
-
-        if (account.auth(menuI)) {
-
-
-            System.out.println("Welcome " + account.name + "\n 1. Deposit \n 2. Withdraw");
-            menuI = input.nextInt();
-
             mainMenu();
-
-        } else if (menuI == 0) {
-            createAccount();
-        } else
-        {
-            System.out.println("Account Doesnt Match any please try again");
         }
-
-        mainMenu();
+        else
+        {
+            System.out.println("No Account found please make one below");
+            createAccount();
+            mainMenu();
+        }
 
     } //run function
 
 
     public void mainMenu()
     {
-        System.out.println("Welcome " + account.name + "\n 1. Deposit \n 2. Withdraw");
+        int amount = 0;
+        int toaccount; //account number to send to
+        System.out.println("Welcome: " + account.name);
+        System.out.println("==========\n 1.Deposit\n 2.Withdraw\n 3.Send Money\n 4.Check Balance\n 5.Check Details\n 6.Logout\n==========");
         menuI = input.nextInt();
-        int amount;
 
-        while (menuI != 0) {
+        while(menuI != 7)
+        {
             switch (menuI) {
                 case 1:
-                    System.out.println("How much do you want to deposit?:");
-                    amount = input.nextInt();
-                    input.nextLine();
+                    //amount input
                     account.deposit(amount);
-                    System.out.println("Your new balance is: " + account.bal);
                     break;
                 case 2:
-                    System.out.println("How much do you want to withdraw?:");
-                    amount = input.nextInt();
-                    input.nextLine();
+                    //amount input
                     account.withdraw(amount);
-                    System.out.println("Your new balance is: " + account.bal);
                     break;
                 case 3:
-                    System.out.println("===========\nTransaction History:");
-                    account.transactionHistory();
-                    System.out.println("===========");
+                    //amount input
+                    //account number to send to
+                    //sendmoney();
                     break;
                 case 4:
-                    System.out.println("Account Number is: " + account.checkAccount());
+                    account.checkBalance();
                     break;
                 case 5:
-                    listAccounts();
+                    System.out.println(account.checkAccount());
                     break;
                 case 6:
+                    logout();
+                    break;
+                case 99:
                     createAccount();
                     break;
-                default:
-                    System.out.println("Error");
+                case 98:
+                    listAccounts();
                     break;
-
             }
-            System.out.println("Welcome " + account.name + "\n 1. Deposit \n 2. Withdraw");
+            System.out.println("Welcome: " + account.name);
+            System.out.println("==========\n 1.Deposit\n 2.Withdraw\n 3.Send Money\n 4.Check Balance\n 5.Check Details\n 6.Logout\n==========");
             menuI = input.nextInt();
         }
     } // Main menu function
+
+    public Boolean login()
+    {
+        //checking if there is any accounts
+        System.out.println("Welcome please enter Account Number: ");
+        int inputNumber = input.nextInt();
+        input.nextLine();
+
+        for(Accounts acc: accountsList)
+        {
+            if(acc.account_No == inputNumber)
+            {
+                account = acc;
+                return true;
+            }
+        }
+        return false;
+    } //login function
+
+
+    public void logout()
+    {
+        run();
+    } //logout function
 
 
     public void listAccounts()
