bcbac4e9870a2db9ff8a18567702a48fdc230480
caseysims <caseysims07@gmail.com>
2026-02-03 20:29:58 +0000

    Refactoring Some methods in Banking class to pave the way for GUI implementation

--- src/Banking.java
+++ src/Banking.java
@@ -1,4 +1,5 @@
 import java.io.File;
+import java.util.Objects;
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.io.FileWriter;
@@ -9,14 +10,10 @@ import java.util.Random;
 public class Banking
 {
 
-    Accounts account;
-    Scanner input;
-    Random rand;
-    ArrayList<Accounts> accountsList = new ArrayList<>();
-
-    int menuI;
-
-
+    private Accounts account;
+    private Scanner input;
+    private Random rand;
+    private ArrayList<Accounts> accountsList = new ArrayList<>();
 
 
     public Banking()
@@ -30,148 +27,53 @@ public class Banking
 
     public static void main(String[] args)
     {
-//        String inname = "JohnClyde";
-//        account = new Accounts(87692,inname,0);
-//        accountsList.add(account);
-//        updateAccountsList();
         Banking bank = new Banking();
-
-
-
-        bank.run();
-
-
     } //main function
 
+    //GETTERS
 
 
-    public void createAccount()
+    public Accounts getAccount() {
+        return account;
+    } // Accounts Getter
+
+    public ArrayList<Accounts> getAccountsList() {
+        return accountsList;
+    } // Accounts List Getter
+
+    public int createAccount(String name, String pin)
     {
         rand = new Random();
-        System.out.println("Welcome to WIS Banking, Please type in your name:");
-        String name = input.nextLine();
         int accountN = rand.nextInt(90000) + 1000;
-        System.out.println("Please input a pin:");
-        String pin = input.nextLine();
-
         account = new Accounts(accountN,name,0,pin);
         accountsList.add(account);
         updateAccountsList();
 
-        System.out.println("Your Details are as follows. \nAccount Number: " + account.getAccount_No());
+        return accountN;
     }//create account function
 
-    public void run()
-    {
-
-
-        if(login() == true)
-        {
-            mainMenu();
-        }
-        else
-        {
-            System.out.println("No Account found please make one below");
-            createAccount();
-            mainMenu();
-        }
-
-    } //run function
-
-
-    public void mainMenu()
-    {
-        int amount = 0;
-        int toaccount; //account number to send to
-        System.out.println("Welcome: " + account.getName());
-        System.out.println("==========\n 1.Deposit\n 2.Withdraw\n 3.Send Money\n 4.Check Balance\n 5.Check Details\n 6.Logout\n==========");
-        menuI = input.nextInt();
-        input.nextLine();
-
-        while(menuI != 7)
-        {
-            switch (menuI) {
-                case 1:
-                    System.out.println("How much do you want to deposit:");
-                    amount = input.nextInt();
-                    input.nextLine();
-                    account.deposit(amount);
-                    updateAccountsList();
-
-                    break;
-                case 2:
-                    System.out.println("How much do you want to withdraw:");
-                    amount = input.nextInt();
-                    input.nextLine();
-                    account.withdraw(amount);
-                    updateAccountsList();
-                    break;
-                case 3:
-                    System.out.println("What account do you want to send to:");
-                    toaccount = input.nextInt();
-                    input.nextLine();
-                    System.out.println("How much do you want to send:");
-                    amount = input.nextInt();
-                    input.nextLine();
-                    sendmoney(amount,toaccount);
-
-                    break;
-                case 4:
-                    System.out.println("£" + account.checkBalance());
-                    break;
-                case 5:
-                    System.out.println(account.checkAccount());
-                    System.out.println("Pin: " + account.getPin());
-                    account.transactionHistory();
-
-                    break;
-                case 6:
-                    logout();
-                    break;
-                case 99:
-                    createAccount();
-                    break;
-                case 98:
-                    listAccounts();
-                    break;
-            }
-            System.out.println("Welcome: " + account.getName());
-            System.out.println("==========\n 1.Deposit\n 2.Withdraw\n 3.Send Money\n 4.Check Balance\n 5.Check Details\n 6.Logout\n==========");
-            menuI = input.nextInt();
-            input.nextLine();
-        }
-    } // Main menu function
-
-    public Boolean login()
+    public Boolean login(int inputNumber, String pinN)
     {
         //checking if there is any accounts
         loadAccounts();
-        if(account == null)
-        {
-            createAccount();
-        }
-        System.out.println("Welcome please enter Account Number: ");
-        int inputNumber = input.nextInt();
-        input.nextLine();
+        //check account did go here
+
+        //enter account number
 
         for(Accounts acc: accountsList)
         {
             if(acc.getAccount_No() == inputNumber)
             {
-                System.out.println("Please Enter Pin Number: ");
-                String PinN = input.nextLine();
-                for(Accounts acc1 : accountsList)
+                //enter pin number
+
+                if(acc.getPin().equals(pinN))
                 {
-                    if(acc1.getPin() == PinN)
-                    {
-                        account = acc;
-                        return true;
-                    }
-                    else
-                    {
-                        System.out.println("Wrong Pin Number");
-                        return false;
-                    }
+                    account = acc;
+                    return true;
+                }
+                else
+                {
+                    return false;
                 }
 
             }
@@ -182,20 +84,14 @@ public class Banking
 
     public void logout()
     {
-        run();
+        account = null;
     } //logout function
 
 
-    public void listAccounts()
-    {
-        for(Accounts t: accountsList)
-        {
-            System.out.println("Account Number: " + t.getAccount_No() + " Name: " + t.getName());
-        }
-    } //list accounts function
 
 
-    public void sendmoney(int amount, int accountnumber)
+
+    public String sendmoney(int amount, int accountnumber)
     {
         Accounts recipient = null;
 
@@ -212,26 +108,26 @@ public class Banking
         {
             if(account.withdraw(amount) == 1)
             {
-                System.out.println("Not enough funds to transfer.");
+                return "Failure: Not enough Funds";
             }
             else
             {
                 recipient.deposit(amount);
                 updateAccountsList();
-                System.out.println("Transaction Succesfull");
+                return "Transaction Successful";
             }
 
 
         }
         else
         {
-            System.out.println("Failure");
+            return "Failure: Couldn't Find Account";
         }
 
     } //send money function
 
 
-    public void updateAccountsList()
+    public String updateAccountsList()
     {
         try(FileWriter writer = new FileWriter("AccountsList.txt"))
         {
@@ -242,12 +138,13 @@ public class Banking
         }
         catch (IOException e)
         {
-            System.out.println("An Error has occurred");
+            return "An Error has Occured";
         }
+        return "Success";
     }// update accounts list function
 
 
-    public void loadAccounts()
+    public String loadAccounts()
     {
 
         File accL = new File("AccountsList.txt");
@@ -265,15 +162,8 @@ public class Banking
         }
         catch (FileNotFoundException e)
         {
-            System.out.println("File Not Found");
+            return "File Not Found";
         }
-
+        return "Success";
     }
-
-
-
-
-
-
-
 } // Banking Class
